8a4eedec9f4daa32cf1be28a54021ab443cace28
Diego Vitor Santos Costa <diegovitorsantoscosta2017@gmail.com>
2021-02-23 09:59:18 -0300

    colocando as imagens real

--- efects.js
+++ efects.js
@@ -4,23 +4,37 @@ class efect{
 
     }
     begin(){
-        
          let element = document.querySelectorAll('.c1')
          let boxs = document.querySelectorAll('.square')
          let boxsLeft = document.querySelectorAll('.left')
          let h1 = document.querySelectorAll('.h1')
+         this.logo = document.querySelector('.logoTop')
 
         //  let carousel = document.querySelector('.carousel')
 
-         this.ExtractExecut(element,'off1','on1');
+         this.ExtractExecut(element,'off7','on7');
          this.ExtractExecut(boxs,'off1','on1');
          this.ExtractExecut(boxsLeft,'off2','on2');
          this.ExtractExecut(h1,'off2','on2')
-         
-        
-             
- 
-         
+
+
+        //  para navbar melhorar o codico dps
+        let check = document.querySelector('#check')
+        this.ul = document.querySelector('.ulHeader')
+        this.sobe()
+
+            addEventListener('click',()=>{
+
+                if(!check.checked){
+
+                  this.sobe()
+                }
+                else{
+                
+                    this.desse()
+                    
+                }
+             })
     }
 
     // Add Efect
@@ -42,6 +56,7 @@ class efect{
 
                if(el.getBoundingClientRect().top < window.innerHeight + 50){
 
+                    
                    this.removeEfect(el,off)
                    this.addEfect(el,on)
                }
@@ -49,6 +64,16 @@ class efect{
             
          });
     }
+
+    sobe(){
+        this.ul.classList.remove('show')
+        this.ul.classList.add('hide')
+    }
+    desse(){
+        this.ul.classList.add('show')
+        this.ul.classList.remove('hide')
+    }
 }
 
 window.efects = new efect
+
